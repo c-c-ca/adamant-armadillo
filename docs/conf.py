3df87daca086b28a3255ec87744afd68f1c72ce6
Adam Carter <adam.clayton.carter@gmail.com>
2026-07-09 22:59:58 -0400

    Update docs/conf.py: fix multiproject_projects path

--- docs/conf.py
+++ docs/conf.py
@@ -2,7 +2,7 @@
 #
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
-import os
+from pathlib import Path
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
@@ -29,8 +29,6 @@ html_theme = "alabaster"
 html_static_path = ["_static"]
 
 PROJECT_ROOT = Path(__file__).resolve().parent.parent
-CORE_LIB_ROOT =  PROJECT_ROOT / "libs" / "adamant-armadillo-core"
-DOMAIN_LIB_ROOT = PROJECT_ROOT / "libs" / "adamant-armadillo-domain"
 
 multiproject_projects = {
     "adamant_armadillo_core": {
